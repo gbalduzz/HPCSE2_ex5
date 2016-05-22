995bef09b9c9698f4bf732d0fb835aef7a5d63b0
gbalduzz <gbalduzz@itp.phys.ethz.ch>
2016-05-22 12:46:21 +0200

    wrote a visual  test of e2e

---
+++ tests/test_binomial.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include"include/expansion/e2e.h"
+using namespace std;
+int main(){
+  cout<<"Binomial(3,2)"<<binomial(3,2)<<endl;
+  cout<<"Binomial(4,4)"<<binomial(4,4)<<endl;
+  cout<<"Binomial(4,2)"<<binomial(4,2)<<endl;
+    cout<<"Binomial(4,2)"<<binomial(4,2)<<endl;
+  
+}
