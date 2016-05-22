a552fe7f4ea4c9c718f46e496b6d81dd8438c027
gbalduzz <gbalduzz@itp.phys.ethz.ch>
2016-05-22 16:15:32 +0200

    spawn tasks for combining expansions. Check how the taskwait works

--- include/expansion/P2E.h
+++ include/expansion/P2E.h
@@ -4,28 +4,40 @@
 #pragma once
 #include "particles.h"
 #include <complex>
-#include <vector>
 #include <cmath>
 
-template <int k>
-void P2E(const Particles& p,double xcom,double ycom,double* c_re,double* c_im){
-    double z_re,z_im; //will store (x+ i y) ^k
-#pragma omp simd
-   for(int i=0;i<k+1;i++) c_re[i]=c_im[i]=0;
-
-    for(int i=0;i<p.N;i++){
-        c_re[0]+=p.w[i];
-      const double x=p.x[i]-xcom, y=p.y[i]-ycom;
-        z_re=1; z_im=0;
-        for(int j=1;j<k+1;j++){
-            const double z_temp=z_re*x-z_im*y;
-            z_im=z_re*p.y[i]+z_im*p.x[i];
-            z_re=z_temp;
-            c_re[j]-=p.w[i]*z_re;
-            c_im[j]-=p.w[i]*z_im;
+template<int k,int K>
+struct sum_kth_coeff{
+        inline static void execute (double* c_re,double* c_im,
+                                    const double x,const double y,const double w,const double z_re,const double z_im){
+          c_re[k]-=w*z_re;
+          c_im[k]-=w*z_im;            //compute z=(x+i y)^k
+          const double new_re=z_re*x-z_im*y;
+          const double new_im=z_re*y+z_im*x;
+          sum_kth_coeff<k+1,K>::execute(c_re,c_im,x,y,w,new_re,new_im);
         }
+};
+
+template<int k>
+struct sum_kth_coeff<k,k>{
+    inline static void execute (double* c_re,double* c_im,
+                                const double x,const double y,const double w,const double z_re,const double z_im){
+        c_re[k]-=w*(z_re*x-z_im*y);
+        c_im[k]-=w*(z_re*y+z_im*x);
+    }//end recursion
+};
+
+template <int k>
+void P2E(const Particles& p,double xcom,double ycom,double* c_re,double* c_im){ //c_re and c_im must have k+1 reserved spaces
+       for(int i=0;i<p.N;i++){
+         const double x=p.x[i]-xcom;
+         const double y=p.y[i]-ycom;
+         const double w=p.w[i];
+         c_re[0]+=w;
+         sum_kth_coeff<1,k>::execute(c_re,c_im,x,y,w,x,y);
     }
     //divide by coeff order
 #pragma omp simd
     for(int i=2;i<k+1;i++) {c_re[i]/=i;c_im[i]/=i;}
+
 }
--- include/expansion/e2p.h
+++ include/expansion/e2p.h
@@ -7,6 +7,8 @@ double e2p(const double z_re,const double z_im,const double* c_re,const double* 
   double result= c_re[0]*0.5*std::log(z_re*z_re+z_im*z_im);
   
   double zk_re=1,zk_im=0;
+  //gcc does unroll this automatically
+  #pragma unroll
   for(int k=1;k<K+1;k++) {
     const double temp=z_re*zk_re-z_im*zk_im;
     zk_im=z_re*zk_im+z_im*zk_re;
