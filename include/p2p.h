1d103226868524a951e55d86155299bd60979338
gbalduzz <gbalduzz@itp.phys.ethz.ch>
2016-05-25 14:01:23 +0200

    Added ispc kernel for p2p

--- include/p2p.h
+++ include/p2p.h
@@ -1,16 +1,8 @@
 #pragma once
-#include <omp.h>
-#include <cmath>
-#include "helper_methods.h"
+#include "p2p.ispc.h"
 
-double p2p( const Particles& p,
+inline double p2p( const Particles& p,
             double xtarget, double ytarget)
 {
-  double res(0);
-#pragma omp simd reduction(+:res)
-//#pragma ivdep
-  for(int i=0;i<p.N;i++){
-   res+=p.w[i]*std::log(squareDistance(p.x[i],xtarget,p.y[i],ytarget));
- }
-  return res/2.;
+  return ispc::p2p(p.x,p.y,p.w,xtarget,ytarget,p.N);
 }
